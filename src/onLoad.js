e8996187d45efdbf5a702a585310c4269bf50b58
sharmavins23 <sharmavins23@gmail.com>
2022-09-05 17:01:34 -0400

    added board refresh code

---
+++ src/onLoad.js
@@ -0,0 +1,96 @@
+// Contains code for loading up the window (and refreshing the game state)
+
+// ===== Constant values =======================================================
+
+var CARDSCALING = 0.6; // Percent sizing for all cards
+
+// ===== Main code and table functionality =====================================
+
+// Refresh the entire game state
+function refreshGameState() {
+    // Start by clearing the table
+    clearTable();
+
+    // Reload aces to the base state
+    placeAces();
+
+    // Place the card backs on the table to align them
+    placeCardBacks();
+}
+
+// Clear the entire table, excluding buttons
+function clearTable() {
+    // Iterate through all rows and columns of the table
+    for (let i = 0; i <= 6; i++) { // Rows
+        for (let j = 0; j <= 4; j++) { // Columns
+            // Get the cell's document ID
+            let cell = document.getElementById("tabler" + i + "c" + j);
+
+            // Remove all children of the cell
+            while (cell.firstChild) {
+                cell.removeChild(cell.firstChild);
+            }
+        }
+    }
+}
+
+// ===== Helper functions ======================================================
+
+// Given some basic information, create a card
+function createCard(cardValue, fill = "") {
+    // Create the card
+    let card = `
+        <svg
+            width="${170 * CARDSCALING}"
+            height="${245 * CARDSCALING}"
+        >
+            <use
+                href="assets/svg-cards.svg#${cardValue}"`;
+
+    // Add the fill if it exists
+    if (fill != "") {
+        card += `fill="${fill}"`;
+    } else {
+        card += `fill="${fill}"`;
+    }
+
+    // Finish the card
+    card += ` transform="scale(${CARDSCALING})"
+            />
+        </svg>`;
+
+    return card;
+}
+
+// ===== Card placement code ===================================================
+
+// Place aces into the base game state
+function placeAces() {
+    // Save suits as an array
+    let suits = ["club", "diamond", "heart", "spade"];
+
+    // Iterate through the suits
+    for (let i = 0; i < suits.length; i++) {
+        // Get the cell's document ID
+        let cell = document.getElementById("tabler6c" + i);
+
+        // Place the ace
+        cell.innerHTML = createCard(`${suits[i]}_1`);
+    }
+}
+
+// Place card backs on the table to align cells
+function placeCardBacks() {
+    // Iterate through the table
+    for (let i = 1; i <= 6; i++) {
+        // Get the cell's document ID
+        let cell = document.getElementById("tabler" + i + "c4");
+
+        // Place the card back
+        cell.innerHTML = createCard("back", "MidnightBlue");
+    }
+
+    // Place the first card as a blank one
+    let cell = document.getElementById("tabler0c4");
+    cell.innerHTML = createCard("blank");
+}
